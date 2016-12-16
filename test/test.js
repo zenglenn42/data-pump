01fe5b5ff9710c122cc264da110e981f41a11b55
zenglenn42 <gsnospampls@pobox.com>
2016-12-15 19:15:12 -0600

    add JavaScript test framework (mocha) add node.js assertion library (chai) add test driver for model.js unit tests
    Allow model unit tests to be run with:
    
    	npm test
    
    from the root directory.

---
+++ test/test.js
@@ -0,0 +1,15 @@
+'use strict';
+//---------------------------------------------------------------------------
+// File: test.js
+//
+// Usage: npm test
+//---------------------------------------------------------------------------
+
+var should = require('chai').should();
+var model = require('../assets/js/model.js');
+
+describe('data-pump: model', function () {
+  it('should pass unit tests', function () {
+     model.unitTests().should.equal(true);
+  });
+});
